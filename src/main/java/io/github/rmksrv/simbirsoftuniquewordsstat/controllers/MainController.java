20d35951a0f09b586036b30cd82accce7edcc625
Roman Kosarev <rmksrv@outlook.com>
2021-10-06 00:29:20 +0300

    v0.1

---
+++ src/main/java/io/github/rmksrv/simbirsoftuniquewordsstat/controllers/MainController.java
@@ -0,0 +1,31 @@
+package io.github.rmksrv.simbirsoftuniquewordsstat.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+  @Autowired
+  private ApiController apiController;
+
+  @GetMapping
+  public String index(Model model) throws IOException {
+    return "index";
+  }
+
+  @PostMapping
+  public String indexViewStats(@RequestParam("urlstring") String URLString, Model model) throws IOException {
+    Map<String, Long> wordsFrequency = apiController.wordsFrequency(URLString);
+    model.addAttribute("wordsFrequency", wordsFrequency);
+    return "index";
+  }
+
+}
---
+++ src/main/java/io/github/rmksrv/simbirsoftuniquewordsstat/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package io.github.rmksrv.simbirsoftuniquewordsstat.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringUtils {
+
+  public static List<String> splitMultipleDelimiters(String target, Iterable<Character> delimiters) {
+    String regex = "[" + delimiters.toString() + "]";
+    return Arrays.asList(target.split(regex));
+  }
+
+}
