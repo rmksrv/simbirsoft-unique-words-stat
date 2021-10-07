414b282f4b4eecbeac853602efe082990a54cbb8
Roman Kosarev <rmksrv@outlook.com>
2021-10-07 23:57:34 +0300

    resolving conflicts

--- src/main/java/io/github/rmksrv/simbirsoftuniquewordsstat/controllers/MainController.java
+++ src/main/java/io/github/rmksrv/simbirsoftuniquewordsstat/controllers/MainController.java
@@ -1,14 +1,13 @@
 package io.github.rmksrv.simbirsoftuniquewordsstat.controllers;
 
-import java.net.URL;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class MainController {
